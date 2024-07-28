b2991381abb2799ef01d663dce14a17b0327fe44
Sofía <159220838+Sof1dev@users.noreply.github.com>
2024-07-28 18:31:22 -0300

    add gayify api route

--- README.md
+++ README.md
@@ -2,3 +2,46 @@
 
 This is the backend for [Gayify](https://github.com/Sof1dev/gayify).
 This codebase is meant to run on AWS and needs additional tweaking to run locally.
+
+# How to run locally
+- First install the dependencies, this project uses Bun but you can use any package manager
+
+```
+bun install
+```
+NOTE: the dependency `sharp` may need some additional libraries if you're on linux, check this [guide](https://sharp.pixelplumbing.com/install) if you have any problems.
+
+
+- Change your CORS origin to your API server (defaults to `http://localhost:3000`)
+
+### For NodeJS
+
+Replace the end of the file 
+
+```js
+export const handler = handle(app);
+```
+
+with:
+
+```js
+import { serve } from '@hono/node-server'
+serve(app)
+```
+
+### For Bun
+
+Replace the end of the file 
+
+```js
+export const handler = handle(app);
+```
+
+with:
+
+```js
+export default app
+```
+
+
+Finally, you can run the `src/index.js` with bun or node.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,87 @@
+import { handle } from "hono/aws-lambda";
 import { Hono } from "hono";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import sharp from "sharp";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
+app.use(
+  "/api/*",
+  cors({
+    origin: ["http://localhost:4321"],
+  })
+);
+
+const DEFAULT_TRANSPARENCY = 40;
+const FLAGS = ["lgbt", "trans", "enby", "lesbian", "gay"];
 app.get("/", async (c) => c.text("hello!"));
+app.post("/api/gayify", async (c) => {
+  const formData = await c.req.formData();
+
+  const file = formData.get("file");
+  const selectedFlag = formData.get("flag");
+
+  let transparency;
+  try {
+    transparency = Number.parseInt(
+      formData.get("transparency")?.toString() || ""
+    );
+
+    if (transparency > 100 || transparency <= 0) {
+      transparency = DEFAULT_TRANSPARENCY;
+    }
+  } catch {
+    transparency = DEFAULT_TRANSPARENCY;
+  }
+
+  const sharpFile = sharp(await file.arrayBuffer());
+
+  let fileMetadata;
+
+  try {
+    fileMetadata = await sharpFile.metadata();
+  } catch {
+    return new Response("", {
+      status: 400,
+    });
+  }
+
+  if (!FLAGS.includes(selectedFlag)) {
+    return c.text("Error, flag doesn't exist", 400);
+  }
+
+  const flagPath = path.join(
+    process.cwd(),
+    "flags/",
+    `${selectedFlag}_flag.webp`
+  );
+
+  const flag = fs.readFileSync(flagPath);
+
+  const resizedFlag = await sharp(flag)
+    .resize({
+      height: fileMetadata.height,
+      width: fileMetadata.width,
+      fit: "fill",
+    })
+    .ensureAlpha(transparency / 100)
+    .toBuffer();
+
+  const outputBuffer = await sharp(await file.arrayBuffer())
+    .composite([
+      {
+        input: resizedFlag,
+        blend: "over",
+      },
+    ])
+    .sharpen()
+    .toFormat("webp")
+    .toBuffer();
+
+  c.header("Content-Type", "image/webp");
+  return c.body(outputBuffer);
+});
+
+export const handler = handle(app);
